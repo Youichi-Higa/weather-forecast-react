6b161b3d86ab85493a12ae625deb797167119586
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-07-08 18:33:59 +0900

    那覇に修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import styled from 'styled-components';
 import { Japan } from 'src/components/Japan';
-import type { Forecasts } from 'src/types/Forecasts';
 
 const App = () => {
-  const [forecasts, setForecasts] = useState<Forecasts | undefined>(undefined);
-
-  const url = 'https://weather.tsukumijima.net/api/forecast/city/471010';
-
-  useEffect(() => {
-    axios
-      .get(url)
-      .then((res) => setForecasts(res.data.forecasts))
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
     <SContainer>
       <Japan />
-      {forecasts?.map((forecast) => (
-        <div key={forecast.date}>
-          <p>{forecast.date}</p>
-          <p>{forecast.telop}</p>
-        </div>
-      ))}
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
   text-align: center;
-`;
-
-const SJapan = styled.img`
-  width: 600px;
 `;
 
 export default App;
--- src/components/Japan.tsx
+++ src/components/Japan.tsx
@@ -3,29 +3,24 @@ import axios from 'axios';
 import styled from 'styled-components';
 import { images } from 'src/assets/images';
 import { Card } from './Card';
-import {cityIds} from "src/constants/cityIds";
+import { cityIds } from 'src/constants/cityIds';
 import type { Forecast } from 'src/types/Forecast';
 
 export const Japan = () => {
-  const [sapporo, setSapporo] = useState<Forecast | undefined>(undefined);
-
-  const baseURL = 'https://weather.tsukumijima.net/api/forecast/city/';
-
-  console.log(sapporo);
+  const [naha, setnaha] = useState<Forecast | undefined>(undefined);
 
   useEffect(() => {
+    const baseURL = 'https://weather.tsukumijima.net/api/forecast/city/';
     axios
-      .get(`${baseURL}${cityIds.sapporo.id}`)
-      .then((res) => setSapporo(res.data.forecasts[0]))
+      .get(`${baseURL}${cityIds.naha.id}`)
+      .then((res) => setnaha(res.data.forecasts[0]))
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <SContainer>
       <SJapan src={images.japan} alt="日本全体" />
-      <SNaha>
-        {sapporo && <Card city={cityIds.sapporo.name} imageUrl={sapporo.image.url} />}
-      </SNaha>
+      <SNaha>{naha && <Card city={cityIds.naha.name} imageUrl={naha.image.url} />}</SNaha>
     </SContainer>
   );
 };
