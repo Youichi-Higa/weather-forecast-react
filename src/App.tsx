1d5498ceb988b62bb9bf9b1a8ad6f09b75447ad9
Youichi-Higa <yo1_1023@yahoo.co.jp>
2022-07-11 00:37:47 +0900

    タイトル・背景

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
+import { images } from 'src/assets/images';
 import { path } from 'src/constants/';
 import {
   Chubu,
@@ -17,8 +18,9 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <SContainer>
+    <SContainer>
+      <STitle>天気予報</STitle>
+      <BrowserRouter>
         <Routes>
           <Route path={path.japan} element={<Japan />} />
           <Route path={path.hokkaido} element={<Hokkaido />} />
@@ -32,13 +34,32 @@ const App = () => {
           <Route path={path.weatherDetail} element={<WeatherDetail />} />
           <Route path={path.notFound} element={<NotFound />} />
         </Routes>
-      </SContainer>
-    </BrowserRouter>
+      </BrowserRouter>
+    </SContainer>
   );
 };
 
 const SContainer = styled.div`
   text-align: center;
+  width: 100%;
+  height: 100vh;
+  background: #2980b9; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to bottom,
+    #2980b9,
+    #6dd5fa,
+    #ffffff
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to bottom,
+    #2980b9,
+    #6dd5fa,
+    #ffffff
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
+const STitle = styled.h1`
+  padding: 20px;
 `;
 
 export default App;
